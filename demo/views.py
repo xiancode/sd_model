cf0366657ab75e7349c55a5ebe28976db434b961
Jay@linux <shizhongxian@126.com>
2016-01-06 09:52:45 +0800

    delete notes add create func to SdViewSet

--- demo/views.py
+++ demo/views.py
@@ -98,61 +98,27 @@ class SdViewSet(viewsets.ModelViewSet):
     queryset = Sdmodel.objects.all()
     serializer_class = SdmodelSerializer
     
-@api_view(['GET','POST'])
-# def sdcal_list(request):
-#     """
-#     
-#     """
-#     if request.method == 'GET':
-#         querysets = Sdmodel.objects.all()
-#         serializer = SdmodelSerializer(querysets,many=True)
-#         return Response(serializer.data)
-#     elif request.method == 'POST':
-#         serializer = SdmodelSerializer(data=request.data)
-#         if serializer.is_valid():
-#             serializer.save()
-#             return Response(serializer.data, status=status.HTTP_201_CREATED)
-#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    
-# class CalList(APIView):
-#     '''
-#     
-#     '''
-#     def get(self,request,format=None):
-#         querysets = Sdmodel.objects.all()
-#         serializer = SdmodelSerializer(querysets,many=True)
-#         return Response(serializer.data)
-#     
-#     def post(self,request,format=None):
-#         serializer = SdmodelSerializer(data=request.data)
-#         if serializer.is_valid():
-#             serializer.save()
-#             return Response(serializer.data, status=status.HTTP_201_CREATED)
-#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    
-# class CalListOne(mixins.ListModelMixin,
-#                                  mixins.CreateModelMixin,
-#                                  generics.GenericAPIView):
-#     queryset = Sdmodel.objects.all()
-#     serializer_class = SdmodelSerializer
-#     
-#     def get(self,request,*args,**kwargs):
-#         return self.list(request,*args,**kwargs)
-#     def post(self,request,*args,**kwargs):
-#         return self.create(request,*args,**kwargs)    
-
-# class LargeResultsSetPagination(PageNumberPagination):
-#     page_size = 10
-#     page_size_query_param = 'page_size'
-#     max_page_size = 10
-#     
-# class CalListTwo(viewsets.ModelViewSet):
-#     authentication_classes = (SessionAuthentication, BasicAuthentication)
-#     permission_classes = (IsAuthenticated,)
-#     queryset = Sdmodel.objects.all()
-#     serializer_class = SdmodelSerializer
-    
-    
+    def create(self,request,format=None):
+        table = request.data['table']
+        result_filename = None
+        data = request.data.copy()
+        logger.info("New request")
+        data["created_time"] = get_now_time()
+        data['rand_fname'] = random_num_str()
+        serializer = SdmodelSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            seria_data = serializer.data
+            date_str = seria_data.get("created")
+            time_str = seria_data.get("created_time")
+            sdmethod = seria_data.get("sdmethod")
+            rand_fname = seria_data.get("rand_fname")
+            save_filename = generate_file_from_timestr(date_str,time_str, BASE_DIR + os.path.sep + "data",rand_fname)
+            json_file(table, save_filename)
+            cal_result = model_cal(seria_data, sdmethod, save_filename, result_filename)
+            return Response(cal_result, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+ 
 
 class ApiViewSet(APIView):
     authentication_classes = (BasicAuthentication,SessionAuthentication )
