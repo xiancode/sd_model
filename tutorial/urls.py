91ecfcc39bc0d9ff6035277e377b2c9c7ec2f4cc
Jay@linux <shizhongxian@126.com>
2016-01-06 11:06:50 +0800

    CSRF verification failed  bugfix : add as_view() to ApiView.

--- tutorial/urls.py
+++ tutorial/urls.py
@@ -27,6 +27,6 @@ urlpatterns = [
     url(r'^', include(router.urls)),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
-    url(r'^api/$', views.ApiViewSet,name="sd_api"),
+    url(r'^api/$', views.ApiViewSet.as_view(),name="sd_api"),
     #url(r'^calculated_list_one',views.CalListOne),
 ]
